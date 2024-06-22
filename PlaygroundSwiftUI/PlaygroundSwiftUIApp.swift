afbca1e8ed3d307c14dc25e8466cbaf5651166a0
takumi <takumi.sakai.engineer@gmail.com>
2024-06-23 00:29:58 +0900

    Initial Commit

---
+++ PlaygroundSwiftUI/PlaygroundSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundSwiftUIApp.swift
+//  PlaygroundSwiftUI
+//
+//  Created by Takumi Sakai on 2024/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
